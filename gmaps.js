3f97fdfd12dd031a5b1442eed187fd595ac9d80e
malilasage <malilaclearwater@gmail.com>
2016-10-29 16:17:31 -0600

    google maps script in external js file. use my location good

--- gmaps.js
+++ gmaps.js
@@ -1,3 +1,40 @@
 $(document).ready(function () {
   console.log('suh dude');
+  initMap();
+
+  // find my location geolocation
+  var findMeButton = $('.find-me');
+    findMeButton.on('click', function(event) {
+      event.preventDefault();
+      console.log('finding location');
+      if (!navigator.geolocation) {
+        findMeButton.addClass("disabled");
+        $('.no-browser-support').addClass("visible");
+      }
+      else {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          // Get the coordinates of the current position.
+          var lat = position.coords.latitude;
+          var lng = position.coords.longitude;
+
+          $('.latitude').text(lat.toFixed(3));
+          $('.longitude').text(lng.toFixed(3));
+          $('.coordinates').addClass('visible');
+          });
+        }
+    });
+
 })
+
+//load google maps api
+function initMap() {
+  var uluru = {lat: -25.363, lng: 131.044};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
--- table.js
+++ table.js
@@ -1,28 +1,27 @@
-$(document).ready(function () {
-
-  if(document.location.search) {
-    var queryString = document.location.search.replace('?', '');
-    var pairs = queryString.split('&').map(function (pair) {
-      return pair.split('=');
-    });
-
-    $('.query-params').show();
-    var $tbody = $('.query-params tbody');
-
-    pairs.forEach(function (pair) {
-      var $tr = $('<tr>');
-      $tr.append($('<td>').html(pair[0]));
-      $tr.append($('<td>').html(pair[1]));
-      $tbody.append($tr);
-    });
-  }
-
-});
+// $(document).ready(function () {
+//
+//   if(document.location.search) {
+//     var queryString = document.location.search.replace('?', '');
+//     var pairs = queryString.split('&').map(function (pair) {
+//       return pair.split('=');
+//     });
+//
+//     $('.query-params').show();
+//     var $tbody = $('.query-params tbody');
+//
+//     pairs.forEach(function (pair) {
+//       var $tr = $('<tr>');
+//       $tr.append($('<td>').html(pair[0]));
+//       $tr.append($('<td>').html(pair[1]));
+//       $tbody.append($tr);
+//     });
+//   }
+//
+// });
 
 
 $(document).ready(function() {
 var findMeButton = $('.find-me');
-
 // from using geolocation APIs mdnhttps://developer.mozilla.org/en-US/docs/Web/API/Geolocation/Using_geolocation#Prompting_for_permission
 // Check if the browser has support for the Geolocation API
 if (!navigator.geolocation) {
@@ -32,12 +31,11 @@ if (!navigator.geolocation) {
 
 } else {
 
-  findMeButton.on('click', function(e) {
+  findMeButton.on('click', function(event) {
 
-    e.preventDefault();
-
+    event.preventDefault();
+    console.log('finding location');
     navigator.geolocation.getCurrentPosition(function(position) {
-
       // Get the coordinates of the current position.
       var lat = position.coords.latitude;
       var lng = position.coords.longitude;
